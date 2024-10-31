78134d6d78c1b13217e556f080d578b05c6446d2
Pacanskiy111 <63151388+Pacanskiy111@users.noreply.github.com>
2024-10-31 10:58:39 +0600

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = 'Ruslan'
+print(name, type(name))
+age = 22
+print(age, type(age))
+new_age = age + 1
+print(new_age, type(new_age))
+is_student = True
+print(is_student, type(is_student))
+print(f"Name: {name}\nAge: {age}\nNew Age: {new_age}\nIs Student: {is_student}")
