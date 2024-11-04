002bccbd00542177fc7babb6347c49a192f5652a
Pacanskiy111 <63151388+Pacanskiy111@users.noreply.github.com>
2024-11-04 18:03:27 +0600

    Update and rename module_1_6.py to variables.py

---
+++ variables.py
@@ -0,0 +1,5 @@
+number_of_completed_tasks = 12
+number_of_hours_used = 1.5
+course_name = "Python"
+time_on_task = number_of_hours_used / number_of_completed_tasks
+print(f"Курс: {course_name}, всего задач: {number_of_completed_tasks}, затрачено часов: {number_of_hours_used}, среднее время выполнения: {time_on_task}.")
