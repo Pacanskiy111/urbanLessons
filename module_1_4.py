e9b1b48e69a9a514250becf16b8a6bdae477b561
Pacanskiy111 <63151388+Pacanskiy111@users.noreply.github.com>
2024-11-04 18:06:00 +0600

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,17 @@
+my_string = input("Кол-во симоволов: --> ")
+print(len(my_string))
+
+my_string = input("Верхний регистр: --> ")
+print(my_string.upper())
+
+my_string = input("Нижний регистр: --> ")
+print(my_string.lower())
+
+my_string = input("Удаление пробелов: --> ")
+print(my_string.replace(" ", ""))
+
+my_string = input("Первый символ: --> ")
+print(my_string[0])
+
+my_string = input("Последний символ: --> ")
+print(my_string[-1])
